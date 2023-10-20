f69c8554748514b6661aea753a215f151ff49bfc
zainebkhlifi1 <zaineb.khlifi@esprit.tn>
2023-10-20 15:39:42 +0100

    first commit

---
+++ src/EspritArrayList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class EspritArrayList implements University {
+
+    EspritArrayList EspritArrayList = new EspritArrayList();
+
+
+    public void ajouterEtudiant(Etudiant e){
+
+
+    }
+
+
+
+
+}
--- src/University.java
+++ src/University.java
@@ -1,18 +1,12 @@
 public interface University {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void ajouterEtudiant(Etudiant e);
+    public boolean rechercherEtudiant(Etudiant e);
+    public boolean rechercherEtudiant(String nom);
+    public void supprimerEtudiant(Etudiant e);
+    public void displayEtudiants();
+    public void trierEtudiantsParId();
+    public void trierEtudiantsParNom();
 
 
 }
